df9bb43fc3730f9038115148e4d04a39131fe979
GOGO Christian <gogochristian009@gmail.com>
2020-12-30 13:00:01 +0100

    premier commit du projet procare

---
+++ asset/script.js
@@ -0,0 +1,32 @@
+$(function(){
+    $('.li1').mouseenter(function(){
+        $('.link1').css('color', 'white');  
+    });
+    $('.li1').mouseleave(function(){
+        $('.link1').css('color', 'black');  
+    });
+    $('.li2').mouseenter(function(){
+        $('.link2').css('color', 'white');  
+    });
+    $('.li2').mouseleave(function(){
+        $('.link2').css('color', 'rgba(0,0,0,0.5)');  
+    });
+    $('.li3').mouseenter(function(){
+        $('.link3').css('color', 'white');  
+    });
+    $('.li3').mouseleave(function(){
+        $('.link3').css('color', 'rgba(0,0,0,0.5)');  
+    });
+    $('.li4').mouseenter(function(){
+        $('.link4').css('color', 'white');  
+    });
+    $('.li4').mouseleave(function(){
+        $('.link4').css('color', 'rgba(0,0,0,0.5)');  
+    });
+    $('.li5').mouseenter(function(){
+        $('.link5').css('color', 'white');  
+    });
+    $('.li5').mouseleave(function(){
+        $('.link5').css('color', 'rgba(0,0,0,0.5)');  
+    });
+});
